56564d3e217c2d179a9042b51a5246e32d68f0f9
Rriznyk04 <rriznyk@student.touro.edu>
2025-03-27 23:26:51 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Simulate login success
+    return redirect(url_for('home'))
+
+@app.route('/home')
 def home():
-    return "Welcome to ZelleMail Summary!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
